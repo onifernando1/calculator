5af586e2b6fe8550dc5314672e77b29a3b00e9c3
onifernando1 <onifernando1@gmail.com>
2022-06-12 20:15:38 +0100

    If statements for operator function

--- java.js
+++ java.js
@@ -1,15 +1,30 @@
 let result = 0
 
+// Operations
 const add = (a,b) => result = a + b ;
 const subtract = (a,b) => result = a - b ; 
 const multiply = (a,b) => result = a * b ;
 const divide = (a,b) => result = a / b ;
 
-add(1,3)
-console.log(result)
 
-const operation = function(operator, a, b) {
-
-
+//Operation function
+const operation = function(a, operator, b) {
+    if (operator == "+") {
+        add(a,b)
+    }
+    else if (operator == "-") {
+        subtract(a,b)
+    }
+    else if (operator == "*") {
+        multiply(a,b)
+    }
+    else if (operator == "/") {
+        divide(a,b)
+    } else {
+        return "INVALID OPERATOR"
+    }
 
 }
+
+operation(1, "+" , 3)
+alert(result)
