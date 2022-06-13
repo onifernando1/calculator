bb47bc9feb4f7efcdf560e5ef0e7d74aa6c55089
onifernando1 <onifernando1@gmail.com>
2022-06-13 11:29:36 +0100

    Create clear display, clear both, clear memory function, attach to click clear

--- java.js
+++ java.js
@@ -1,4 +1,8 @@
 let result = 0
+const displayContainer = document.querySelector(".displayContainer")
+const display = document.querySelector(".display")
+const memoryDisplay = document.querySelector(".memory")
+
 
 // Operations
 const add = (a,b) => result = a + b ;
@@ -26,5 +30,54 @@ const operation = function(a, operator, b) {
 
 }
 
-// operation(1, "*" , 3)
-// alert(result)
+operation(1, "*" , 3)
+
+
+let displayValue = result;
+console.log({displayValue})
+
+
+// let clear = function(){
+//     const display = document.querySelector(".display")
+//     display.remove()
+// }
+
+let clearDisplay = function(){
+    display.remove()
+}
+
+let clearMemory = function() {
+    memoryDisplay.remove()
+}
+
+let clearBoth = function(){
+    clearDisplay()
+    clearMemory()
+
+}
+
+let newDisplayValue = function(){
+    display.textContent = displayValue
+    const div = document.createElement("div")
+    div.classList.add("display")
+    displayContainer.appendChild(display)
+}
+
+// let newDisplayValue = function(){
+//     const displayContainer = document.querySelector(".displayContainer")
+//     const display = document.createElement("div")
+//     display.classList.add("display")
+//     display.textContent = displayValue
+//     displayContainer.appendChild(display)
+// }
+
+let refresh = function(){
+    clearDisplay()
+    newDisplayValue()
+}
+
+refresh()
+
+
+const clearButton = document.querySelector(".clear")
+clearButton.onclick = () => clearBoth()
