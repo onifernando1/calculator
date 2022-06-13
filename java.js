06953004cb0604c9b6597f70bb4063e1fd6daabb
onifernando1 <onifernando1@gmail.com>
2022-06-13 15:16:28 +0100

    Change on clicks to addeventlisteners

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
-let result = 0
-let memory = 5;
+let result = ''
+let memory = '';
+let currentValue = ''
 const displayContainer = document.querySelector(".displayContainer")
 const display = document.querySelector(".display")
 const memoryDisplay = document.querySelector(".memory")
@@ -85,6 +86,8 @@ let number = function() {
     numbers.forEach((number) => {
         number.addEventListener("click", () => {
             displayValue = number.textContent
+            currentValue = number.textContent
+            alert(`Current value is ${currentValue}`)
             refresh()
         })
         
@@ -95,56 +98,72 @@ let number = function() {
 number()
 
 const clearButton = document.querySelector(".clear")
-clearButton.onclick = () => clearBoth()
+clearButton.addEventListener("click", () => { clearBoth()
+})
 
 const zeroButton = document.querySelector(".zero")
-zeroButton.onclick = () => clearBoth()
+zeroButton.addEventListener("click", () => { number()
+})
 
 const oneButton = document.querySelector(".one")
-oneButton.onclick = () => number()
+oneButton.addEventListener("click", () => { number()
+})
 
 const twoButton = document.querySelector(".two")
-twoButton.onclick = () => number()
+twoButton.addEventListener("click", () => { number()
+})
 
 const threeButton = document.querySelector(".three")
-threeButton.onclick = () => number()
+threeButton.addEventListener("click", () => { number()
+})
 
 const fourButton = document.querySelector(".four")
-fourButton.onclick = () => number()
+fourButton.addEventListener("click", () => { number()
+})
 
 const fiveButton = document.querySelector(".five")
-fiveButton.onclick = () => number()
+fiveButton.addEventListener("click", () => { number()
+})
 
 const sixButton = document.querySelector(".six")
-sixButton.onclick = () => number()
+sixButton.addEventListener("click", () => { number()
+})
 
 const sevenButton = document.querySelector(".seven")
-sevenButton.onclick = () => number()
+sevenButton.addEventListener("click", () => { number()
+})
 
 const eightButton = document.querySelector(".eight")
-eightButton.onclick = () => number()
+eightButton.addEventListener("click", () => { number()
+})
 
 const nineButton = document.querySelector(".nine")
-nineButton.onclick = () => number()
+nineButton.addEventListener("click", () => { number()
+})
 
 const plusButton = document.querySelector(".add")
-plusButton.onclick = () => number()
+plusButton.addEventListener("click", () => { number()
+})
 
 
 const subractButton = document.querySelector(".subtract")
-subractButton.onclick = () => number()
+subractButton.addEventListener("click", () => { number()
+})
 
 
 const multiplyButton = document.querySelector(".multiply")
-multiplyButton.onclick = () => number()
+multiplyButton.addEventListener("click", () => { number()
+})
 
 
 const divideButton = document.querySelector(".divide")
-divideButton.onclick = () => number()
+divideButton.addEventListener("click", () => { number()
+})
 
 
 const decimalButton = document.querySelector(".decimal")
-decimalButton.onclick = () => number()
+decimalButton.addEventListener("click", () => { number()
+})
 
 const equalsButton = document.querySelector(".equals")
 equalsButton.addEventListener("click", () => {newDisplayValue()
@@ -153,11 +172,13 @@ equalsButton.addEventListener("click", () => {newDisplayValue()
 })
 
 const onButton = document.querySelector(".on")
-onButton.onclick = () => number()
+onButton.addEventListener("click", () => { number()
+})
 
 
 const offButton = document.querySelector(".off")
-offButton.onclick = () => number()
+offButton.addEventListener("click", () => { number()
+})
 
 
 // const testButtons = document.querySelectorAll("button")
@@ -168,3 +189,18 @@ offButton.onclick = () => number()
 //     })
 
 // })
+
+
+
+
+/*
+1. click a button (1)
+2. store what they clicked (1)
+3. display what they clicked (1)
+3. click another button (2)
+4. store what they clicked + what they clicked  (12)
+5. display (12)
+
+
+
+*/ 
